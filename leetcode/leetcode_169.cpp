5eb3ced9332d5139416e98c5d57d655d42a8de09
Mahi Sarwar Anol <91027428+Mahi-Anol@users.noreply.github.com>
2022-10-19 19:16:05 +0600

    Update leetcode_169.cpp

--- leetcode/leetcode_169.cpp
+++ leetcode/leetcode_169.cpp
@@ -1,23 +1,11 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+         
+        int maj=-1;
+         int cnt=0;
 
-int main()
-{
-	vector<int> a;
-	int p;
-	cin>>p;
-	for(int i=0;i<p;i++)
-	{
-		int x;
-		cin>>x;
-		a.push_back(x);
-	}
-
-          int maj=-1;
-             int cnt=0;
-
-	      for(auto i=a.begin();i!=a.end();i++)
+	      for(auto i=nums.begin();i!=nums.end();i++)
          {
              if(cnt==0)
              {
@@ -27,8 +15,6 @@ int main()
              else cnt--;
          }
 
-         cout<<maj<<endl;
-
-
-	return 0;
-}
+         return maj;
+    }
+};
