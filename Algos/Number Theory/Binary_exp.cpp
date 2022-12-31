ae9d3f1c97d3bbc8de947947e901dbfc1c55ebbb
Mahi Sarwar Anol <91027428+Mahi-Anol@users.noreply.github.com>
2023-01-01 03:04:42 +0600

    Create Binary_exp.cpp

---
+++ Algos/Number Theory/Binary_exp.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+#define mod 1000000007;
+namespace mahi{
+    long long pow(long long x,long long p)//Receives integer as input
+    {
+         long long result=1;
+         while(p)
+         {
+             if(p&1)
+             {
+                 result=(result*x);
+                 p--;
+             }
+             else
+             {
+                x=(x*x);
+                p>>=1;
+             }
+         }
+         return result;
+    }
+}
+int main()
+{
+    cout<<mahi::pow(10,2)<<endl;
+
+    using namespace mahi;
+
+    cout<<pow(3,4)<<endl;
+
+    return 0;
+}
